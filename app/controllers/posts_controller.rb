dba5bfd50528075cd9b38bd62fc2e4dd67c4b96e
sdonohue0918 <sdonohue0918@gmail.com>
2020-09-15 12:27:54 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,18 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-
+  
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end
+  
+  
   # add create method here
 end
+
+
+# When a user submits a form, it is the params hash that contains all the input data. As long as the form is routed to the create method we've written (in config/routes.rb), we'll be able to initialize a new instance of Post, grab those input values from params, assign them the post instance attributes and save the instance to our database.
